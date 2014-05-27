28e130c18320600b240c36214e1bb1a96db4ddb0
Jassiem Moore <jassiemmoore@gmail.com>
2014-05-27 15:20:04 -0400

    Added api calls and ui for events. Scaffolding for calendar plugin.

--- assets/app/scripts/controllers/event.js
+++ assets/app/scripts/controllers/event.js
@@ -1,13 +1,21 @@
 function EventCtrl($scope, apiService, $routeParams) {
 	$scope.event;
-	console.log('some stuff');
-	apiService.get(function(data, status) {
-		if(status === 200) {
-			$scope.event = data.event;
-		} else {
-			$scope.event = {name: 'Unable to retrieve event', description: ''};
-		}
-	}, '/api/get-event', {event_id: $routeParams.id});
+
+	$scope.init = function() {
+		apiService.get(function(data, status) {
+			if(status === 200) {
+				$scope.event = data.event;
+			} else {
+				$scope.event = {name: 'Unable to retrieve event', description: ''};
+			}
+		}, '/api/get-event', {event_id: $routeParams.id});
+	}
+	$scope.init();
+
+	$scope.addToCalendar = function() {
+		alert('Added!');
+	}
+
 };
 
 EventCtrl.$inject = ['$scope', 'apiService', '$routeParams'];
